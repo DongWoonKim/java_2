6472177ee3e8335a4f7900cef305be155cc37998
DongwoonKim <01030985843z@naver.com>
2025-03-06 09:44:13 +0900

    set up

---
+++ springboot/basic-board-v2/src/main/java/com/example/spring/basicboardv2/controller/MemberApiController.java
@@ -0,0 +1,10 @@
+package com.example.spring.basicboardv2.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MemberApiController {
+
+
+
+}
