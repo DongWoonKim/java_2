fcfbef96af2081c6929e6d4c2f1c7b52054344af
DongwoonKim <01030985843z@naver.com>
2025-03-06 10:54:01 +0900

    sign up

--- springboot/basic-board-v2/src/main/java/com/example/spring/basicboardv2/controller/MemberApiController.java
+++ springboot/basic-board-v2/src/main/java/com/example/spring/basicboardv2/controller/MemberApiController.java
@@ -1,10 +1,28 @@
 package com.example.spring.basicboardv2.controller;
 
+import com.example.spring.basicboardv2.dto.SignUpRequestDTO;
+import com.example.spring.basicboardv2.dto.SignUpResponseDTO;
+import com.example.spring.basicboardv2.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class MemberApiController {
 
+    private final MemberService memberService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
+    @PostMapping("/join")
+    public SignUpResponseDTO signUp(@RequestBody SignUpRequestDTO signUpRequestDTO) {
+        System.out.println(signUpRequestDTO);
+        memberService.signUp(signUpRequestDTO.toMember(bCryptPasswordEncoder));
+        return SignUpResponseDTO.builder()
+                .successed(true)
+                .build();
+    }
 
 }
