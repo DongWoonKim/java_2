d1af7f548a97f0950ec9037715be648effaa5bac
DongwoonKim <01030985843z@naver.com>
2025-03-07 12:41:50 +0900

    board list

--- springboot/basic-board-v2/src/main/java/com/example/spring/basicboardv2/mapper/BoardMapper.java
+++ springboot/basic-board-v2/src/main/java/com/example/spring/basicboardv2/mapper/BoardMapper.java
@@ -1,9 +1,14 @@
 package com.example.spring.basicboardv2.mapper;
 
 import com.example.spring.basicboardv2.model.Article;
+import com.example.spring.basicboardv2.model.Paging;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface BoardMapper {
     void saveArticle(Article article);
+    List<Article> getArticles(Paging page);
+    int getArticleCnt();
 }
