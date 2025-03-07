ce9844393a3dfa178d59353004ca2ce12da96161
DongwoonKim <01030985843z@naver.com>
2025-03-07 11:14:38 +0900

    write

--- springboot/basic-board-v2/src/main/java/com/example/spring/basicboardv2/controller/BoardApiController.java
+++ springboot/basic-board-v2/src/main/java/com/example/spring/basicboardv2/controller/BoardApiController.java
@@ -1,12 +1,28 @@
 package com.example.spring.basicboardv2.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/board")
 public class BoardApiController {
 
-
+    @PostMapping
+    public void saveArticle(
+            @RequestParam("title") String title,
+            @RequestParam("content") String content,
+            @RequestParam("hiddenUserId") String userId,
+            @RequestParam("file") MultipartFile file
+    ) {
+        System.out.println(title);
+        System.out.println(content);
+        System.out.println(userId);
+        System.out.println(file.getOriginalFilename());
+    }
 
 }
