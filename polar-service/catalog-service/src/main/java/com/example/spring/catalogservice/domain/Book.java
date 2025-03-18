11eeb01147b1e0f717e24c88cacc05a42f20f05e
DongwoonKim <01030985843z@naver.com>
2025-03-18 11:53:48 +0900

    catalog service

--- polar-service/catalog-service/src/main/java/com/example/spring/catalogservice/domain/Book.java
+++ polar-service/catalog-service/src/main/java/com/example/spring/catalogservice/domain/Book.java
@@ -1,11 +1,25 @@
 package com.example.spring.catalogservice.domain;
 
 import lombok.Builder;
+import org.springframework.data.annotation.*;
+import org.springframework.data.relational.core.mapping.Column;
+
+import java.time.Instant;
 
 @Builder
 public record Book(
+        @Id
+        Long id,
         String isbn,
         String author,
         String title,
-        Double price
+        Double price,
+        @Column("create_at")
+        @CreatedDate
+        Instant createAt,
+        @Column("last_modified_at")
+        @LastModifiedDate
+        Instant lastModifiedAt,
+        @Version
+        int version
 ) {}
--- polar-service/catalog-service/src/main/java/com/example/spring/catalogservice/domain/BookRespository.java
+++ polar-service/catalog-service/src/main/java/com/example/spring/catalogservice/domain/BookRespository.java
@@ -1,14 +1,19 @@
 package com.example.spring.catalogservice.domain;
 
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Repository
-public interface BookRespository {
-    Iterable<Book> findAll();
+public interface BookRespository extends CrudRepository<Book, Long> {
     Optional<Book> findByIsbn(String isbn);
-    Book save(Book book);
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM book WHERE isbn:isbn")
     void deleteByIsbn(String isbn);
     boolean existsByIsbn(String isbn);
 }
