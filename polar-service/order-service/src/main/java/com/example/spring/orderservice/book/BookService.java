78a1572871819e1ec41a6a910cbe3134f9f714e7
DongwoonKim <01030985843z@naver.com>
2025-03-24 12:12:34 +0900

    webclient crud

---
+++ polar-service/order-service/src/main/java/com/example/spring/orderservice/book/BookService.java
@@ -0,0 +1,7 @@
+package com.example.spring.orderservice.book;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+}
---
+++ polar-service/order-service/src/main/java/com/example/spring/orderservice/order/web/BookController.java
@@ -0,0 +1,4 @@
+package com.example.spring.orderservice.order.web;
+
+public class BookController {
+}
