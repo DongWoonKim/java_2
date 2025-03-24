8c022a125d7d77522866b14900d754824e6469fb
DongwoonKim <01030985843z@naver.com>
2025-03-24 12:23:17 +0900

    service

--- polar-service/order-service/src/main/java/com/example/spring/orderservice/book/Book.java
+++ polar-service/order-service/src/main/java/com/example/spring/orderservice/book/Book.java
@@ -1,9 +1,12 @@
 package com.example.spring.orderservice.book;
 
+import lombok.Builder;
+
+@Builder
 public record Book(
         String isbn,
         String title,
         String author,
-        Double price
-) {
-}
+        Double price,
+        String message
+) {}
