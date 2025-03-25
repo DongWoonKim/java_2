37a24d1c0b0d4f1852955731bdb3b3ab85622e10
DongwoonKim <01030985843z@naver.com>
2025-03-25 11:21:18 +0900

    controller

---
+++ auth-service/src/main/java/com/example/spring/authservice/controller/TokenController.java
@@ -0,0 +1,9 @@
+package com.example.spring.authservice.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class TokenController {
+}
