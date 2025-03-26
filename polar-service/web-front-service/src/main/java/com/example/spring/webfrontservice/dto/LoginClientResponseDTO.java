25cffae777aba4e3315e43733d69b6aa044dff80
DongwoonKim <01030985843z@naver.com>
2025-03-26 10:41:31 +0900

    auth client

---
+++ polar-service/web-front-service/src/main/java/com/example/spring/webfrontservice/dto/LoginClientResponseDTO.java
@@ -0,0 +1,14 @@
+package com.example.spring.webfrontservice.dto;
+
+import lombok.Getter;
+
+@Getter
+public class LoginClientResponseDTO {
+    private boolean loggedIn;
+    private String userName;
+    private String userId;
+    private String accessToken;
+    private String refreshToken;
+
+
+}
